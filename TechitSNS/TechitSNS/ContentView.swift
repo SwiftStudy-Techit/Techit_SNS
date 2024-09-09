d56974440612144cc3fb02fb123a7cbfce8ee47b
kim dong kyung <kdkd422@naver.com>
2024-09-09 14:16:47 +0900

    init 프로젝트

---
+++ TechitSNS/TechitSNS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TechitSNS
+//
+//  Created by 김동경 on 2024/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TechitSNS/TechitSNS/TechitSNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechitSNSApp.swift
+//  TechitSNS
+//
+//  Created by 김동경 on 2024/09/09.
+//
+
+import SwiftUI
+
+@main
+struct TechitSNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
