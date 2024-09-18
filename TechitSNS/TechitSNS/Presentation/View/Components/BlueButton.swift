2d6e7ebe7762f21cbae0d0467fcfa90c90b4b813
kim dong kyung <kdkd422@naver.com>
2024-09-18 22:43:29 +0900

    Add blue색상 컴포넌트 추가

--- TechitSNS/TechitSNS/Presentation/View/Components/BlueButton.swift
+++ TechitSNS/TechitSNS/Presentation/View/Components/BlueButton.swift
@@ -5,4 +5,32 @@
 //  Created by 김동경 on 2024/09/11.
 //
 
-import Foundation
+import SwiftUI
+
+struct BlueButton<Content: View> : View {
+    
+    var disabled: Bool
+    let action: () -> Void
+    let label: Content
+    
+    init(disabled: Bool, action: @escaping () -> Void, @ViewBuilder content: () -> Content) {
+        self.disabled = disabled
+        self.action = action
+        self.label = content()
+    }
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+           label
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity)
+                .background(!disabled ? .blue : .gray)
+                .clipShape(RoundedRectangle(cornerRadius: 18))
+                .padding(.horizontal)
+        }
+        .disabled(disabled)
+    }
+}
+
