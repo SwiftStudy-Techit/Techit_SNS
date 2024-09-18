5b8853c872f8532e006c9eaec7190509a0a01b7a
kim dong kyung <kdkd422@naver.com>
2024-09-18 22:44:36 +0900

    Add 피드 뷰 추가

--- TechitSNS/TechitSNS/Presentation/View/Feed/FeedView.swift
+++ TechitSNS/TechitSNS/Presentation/View/Feed/FeedView.swift
@@ -8,11 +8,102 @@
 import SwiftUI
 
 struct FeedView: View {
+    
+    @EnvironmentObject private var viewModel: FeedViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(viewModel.feeds, id: \.postId) { feed in
+                        FeedItemView(feed: feed, size: geometry.size.width)
+                    }
+                }
+            }
+            .refreshable {
+                await viewModel.loadFeed()
+            }
+            .task {
+                await viewModel.loadFeed()
+            }
+        }
+    }
+}
+
+
+struct FeedItemView: View {
+    
+    let feed: Post
+    let size: CGFloat
+    
+    var body: some View {
+        VStack {
+            //작성자 정보 섹션
+            HStack {
+                
+                AsyncImage(url: URL(string: feed.writerProfileUrl)) { image in
+                    image
+                        .resizable()
+                        .frame(width: size / 9, height: size / 9)
+                        .clipShape(Circle())
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                Text(feed.writerName)
+                    .foregroundStyle(.primary)
+                    .bold()
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "ellipsis")
+                }
+            }
+            .padding(.horizontal, 18)
+            .padding(.vertical, 3)
+            
+            //사진 섹션
+            TabView {
+                ForEach(feed.imagesUrl, id:\.self) { url in
+                    AsyncImage(url: URL(string: url)) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                }
+            }
+            .frame(height: 300)
+            .frame(maxWidth: size, maxHeight: size) // 최대 높이를 설정
+            .tabViewStyle(.page)
+            
+            
+            //본문 섹션
+            HStack {
+                Text(feed.text)
+                    
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 2)
+            
+            //시간섹션
+            HStack {
+                Text(feed.date.formattedTimeAgo)
+                    .foregroundStyle(.gray)
+                    .font(.caption)
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+        }
     }
 }
 
 #Preview {
     FeedView()
+        .environmentObject(FeedViewModel())
 }
