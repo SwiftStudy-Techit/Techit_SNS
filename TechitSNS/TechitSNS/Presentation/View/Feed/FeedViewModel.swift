b1dadbc22fc9fbcc96dbd0d069f8d3cda39d810f
kim dong kyung <kdkd422@naver.com>
2024-09-18 22:44:55 +0900

    Add 피드 뷰모델 추가

--- TechitSNS/TechitSNS/Presentation/View/Feed/FeedViewModel.swift
+++ TechitSNS/TechitSNS/Presentation/View/Feed/FeedViewModel.swift
@@ -5,4 +5,29 @@
 //  Created by 김동경 on 9/18/24.
 //
 
+import FirebaseFirestore
 import Foundation
+
+
+class FeedViewModel: ObservableObject {
+    @Published var feeds: [Post] = []
+    @Published var isLoading: Bool = false
+    
+   
+    
+    //Feed데이터를 로드하는 함수
+    func loadFeed() async {
+        let db = Firestore.firestore()
+        do {
+            let documents = try await db.collection("Post").order(by: "date", descending: true).getDocuments().documents
+            let post = try documents.compactMap { snapshot in
+                try snapshot.data(as: Post.self)
+            }
+            DispatchQueue.main.async {
+                self.feeds = post
+            }
+        } catch {
+            print("\(error.localizedDescription)")
+        }
+    }
+}
